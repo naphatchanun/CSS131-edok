2a27f7089015f6ac7a026455ff6f722a9a8a007c
naphatchanun <111549617+naphatchanun@users.noreply.github.com>
2024-04-19 01:19:35 +0700

    -
    -

--- client/src/Contact.jsx
+++ client/src/Contact.jsx
@@ -1,22 +1,17 @@
-import React from 'react'
-import Picture from './Images/Picture.jpg'
-import Name from './Images/contact.png'
+import React from "react";
+import Picture from "./Images/Picture.jpg";
+import Name from "./Images/contact.png";
 
 const Contact = () => {
   return (
-    <div className='bg-[#fffded] h-screen'>
-        <div className='grid grid-cols-2'>
-            <div className=' flex justify-center pt-10'>
-                <img src={Picture} alt="" width="300px" height='100px'/>
-            </div>
-            <div className='flex justify-start mt-20 pt-10 h-[200px]'>
-                <img src={Name} alt="" width="600px"  />
-            </div>
-
+    <div className="bg-[#fffded] h-screen">
+      <div className="grid grid-cols-2">
+        <div className=" flex justify-center pt-10">
+          <img src={Picture} alt="" width="300px" height="100px" />
         </div>
-
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Contact
+export default Contact;
